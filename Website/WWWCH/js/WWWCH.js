e59fdad8371e7e56e6258c46381032539a0f78bc
LukeThomas1991 <luke_0702@hotmail.com>
2015-09-12 21:32:54 +0100

    Added Google Translate for Welsh. Static header when header goes onto two lines. Ran CodeMaid on all files.

--- Website/WWWCH/js/WWWCH.js
+++ Website/WWWCH/js/WWWCH.js
@@ -1,5 +1,4 @@
-﻿
-// WOW
+﻿// WOW
 new WOW().init();
 
 // Page scroll
@@ -13,12 +12,6 @@ $(function () {
         event.preventDefault();
     });
 });
-
-$('body').scrollspy({
-    target: '.navbar-fixed-top',
-    offset: 55
-})
-
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function () {
     $('.navbar-toggle:visible').click();
@@ -26,33 +19,24 @@ $('.navbar-collapse ul li a').click(function () {
 
 // Video
 $(function () {
-
     var $allVideos = $("iframe[src^='//player.vimeo.com'], iframe[src^='//www.youtube.com'], object, embed"),
     $fluidEl = $("figure");
 
     $allVideos.each(function () {
-
         $(this)
           // jQuery .data does not work on object/embed elements
           .attr('data-aspectRatio', this.height / this.width)
           .removeAttr('height')
           .removeAttr('width');
-
     });
 
     $(window).resize(function () {
-
         var newWidth = $fluidEl.width();
         $allVideos.each(function () {
-
             var $el = $(this);
             $el
                 .width(newWidth)
                 .height(newWidth * $el.attr('data-aspectRatio'));
-
         });
-
     }).resize();
-
 });
-
